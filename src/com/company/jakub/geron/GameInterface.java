d29630aac9930542026b35f25859a16a7ee01d3b
Kulawy <catatu1992@gmail.com>
2018-05-23 21:12:46 +0200

    first version

---
+++ src/com/company/jakub/geron/GameInterface.java
@@ -0,0 +1,59 @@
+package com.company.jakub.geron;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class GameInterface {
+
+    private SingleGem actualGame;
+    private Scanner scanner;
+
+
+    public GameInterface(){
+        scanner = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
+    }
+
+    public boolean initGem() {
+        Player one = new Player("Server");
+        Player two = new Player("Receiver");
+        actualGame = new SingleGem( one, two);
+        return true;
+    }
+
+    public boolean displayRules() {
+        System.out.println("RULES:");
+        System.out.println("Type ");
+        System.out.println("Server = 1");
+        System.out.println("Receiver = 2");
+        System.out.println("Please enter the player that wins the poin");
+        return true;
+    }
+
+
+    public boolean displayWinner() {
+
+        switch ( actualGame.setWinnerOfGem()){
+            case 1:
+                System.out.println("Server wins");
+                return true;
+            case 2:
+                System.out.println("Receiver wins");
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    public void startGame() {
+        initGem();
+        displayRules();
+
+        while ( !actualGame.isOver() ){
+            System.out.println(actualGame.displayScore());
+            System.out.print("#");
+            actualGame.setBallWinner(scanner.nextInt());
+        }
+        displayWinner();
+    }
+
+}
---
+++ src/com/company/jakub/geron/Main.java
@@ -0,0 +1,13 @@
+package com.company.jakub.geron;
+
+import java.io.InputStream;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        GameInterface myInterface = new GameInterface();
+        myInterface.startGame();
+
+    }
+}
